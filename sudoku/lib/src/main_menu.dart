8b7df8b952117338487681b23d1c5104f8d18392
Beantech0 <holyba.attila@gmail.com>
2023-10-23 21:15:54 +0200

    replace

---
+++ sudoku/lib/src/main_menu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MainWindow extends StatelessWidget  {
+  const MainWindow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)  {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Test'),
+    ),
+    );
+  }
+}
