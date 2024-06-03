e8b024108e609dc787b9714d4d72774a822290fc
bhuaholy <attila.holyba@flex.com>
2024-06-03 15:38:47 +0200

    fix

--- lib/screens/scoreboard.dart
+++ lib/screens/scoreboard.dart
@@ -5,25 +5,36 @@ import 'package:sudoku/widgets/buttons.dart';
 import 'package:sudoku/widgets/images.dart';
 
 class Scoreboard extends StatefulWidget {
-
   const Scoreboard({super.key});
 
   @override
   State<Scoreboard> createState() => _ScoreboardState();
 }
 
-  Future _getHistory() async {
+Future<List<Map<String, dynamic>>> _getHistory() async {
   String loggedUserID = sharedPreferences.getString('loggedUserID') ?? "";
-  final history = await DatabaseHelper.instance
-        .getHistory(userid: int.parse(loggedUserID));
-  print(history);
+  final history =
+      await DatabaseHelper.instance.getHistory(userid: int.parse(loggedUserID));
   return history;
-  }
+}
 
 class _ScoreboardState extends State<Scoreboard> {
   String? loggedUser = sharedPreferences.getString('loggedUser') ?? "";
-  final history = _getHistory();
-  
+  Future<List<Map<String, dynamic>>> history = _getHistory();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadHistory();
+  }
+
+  Future<void> _loadHistory() async {
+    final loadedHistory = await _getHistory();
+    setState(() {
+      history = loadedHistory as Future<List<Map<String, dynamic>>>;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,44 +46,74 @@ class _ScoreboardState extends State<Scoreboard> {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Form(
-              child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              'Szia, $loggedUser',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Szia, $loggedUser',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: FutureBuilder<List<Map<String, dynamic>>>(
+                    future: _getHistory(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return CircularProgressIndicator();
+                      } else if (snapshot.hasError) {
+                        return Text('Hiba történt: ${snapshot.error}');
+                      } else {
+                        final history = snapshot.data!;
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text(
+                              'Play Count: ',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text('${history.length}'),
+                            Text(
+                              'Hard: ',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                              'Medium: ',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                              'Easy: ',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        );
+                      }
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.green),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Text(
+                    'Fastest game:  min',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.green),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Play Count: ',style: TextStyle(fontSize: 20),),
-                  Text('Hard: ',style: TextStyle(fontSize: 20),),
-                  Text('Medium: ',style: TextStyle(fontSize: 20),),
-                  Text('Easy: ',style: TextStyle(fontSize: 20),),
-                ],
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              padding: EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.green),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Text('Fastest game:  min',style: TextStyle(fontSize: 20),),
-            ),
-          ],
-                    ),),
+          ),
         ),
       ),
     );
